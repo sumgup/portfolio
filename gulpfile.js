c716478dfdcd6abc2c51bd1d4a7cd33d44618e6a
sumit gupta <sumit4palz@gmail.com>
2019-12-23 12:30:56 +0530

    Fixing gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
   return gulp.src('./dist/**/*')
-=======
 import { task, src } from 'gulp';
 import ghPages from 'gulp-gh-pages';
 
 task('deploy', function() {
   return src('./dist/**/*')
->>>>>>> develop
     .pipe(ghPages());
 });
