fc4964186a7c4f2b9bf97863f4493f9e3e11f07e
sumit gupta <sumit4palz@gmail.com>
2019-12-23 10:47:46 +0530

    Adding grunt task for scss

---
+++ gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+
+    // A very basic default task.
+    grunt.registerTask('default', 'Log some stuff.', function() {
+        grunt.log.write('Logging some stuff...').ok();
+    });
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner : "/*! index.min.js file */\n"
+            },
+            build: {
+                src : ["js/index.js"],
+                dest : "dist/js/index.js"
+            }
+        },
+        
+        cssmin : {
+            target : {
+                src : ["css/**/*.css"],
+                dest : "dist/css/site.css"
+            }
+        },
+
+        htmlmin: {                                     // Task
+            dist: {                                      // Target
+                options: {                                 // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'dist/index.html': 'index.html',     // 'destination': 'source'
+                    'dist/site.webmanifest': 'site.webmanifest',     // 'destination': 'source'
+                }
+            }
+        },
+
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    {expand: true, src: ['images/*'], dest: 'dist', filter: 'isFile'},
+                   ],
+            },
+        },
+
+        sass: {
+            dist: {
+                options: {                 
+                    compass: true,
+                },
+                files: {
+                    'dist/css/custom.css' : 'scss/custom.scss'
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the task.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task(s).
+    grunt.registerTask('default', ['copy','htmlmin','uglify','cssmin','sass']);
+
+};
