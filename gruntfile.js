24c0505c404e52651953ecd89358d8a80bfec195
sumit gupta <sumit4palz@gmail.com>
2019-12-26 09:40:20 +0530

    Fixing issues with css

--- gruntfile.js
+++ gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
                 files: [
                     // includes files within path
                     {expand: true, src: ['img/*'], dest: 'dist', filter: 'isFile'},
-                    {expand: true, src: ['css/*'], dest: 'dist/css', filter: 'isFile'},
+                    {expand: true, src: ['css/*'], dest: 'dist', filter: 'isFile'},
 
                    ],
             },
