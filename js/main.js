9e2bc63a2fa5ac765e3b8c408f37ae6b05087b18
sumit gupta <sumit4palz@gmail.com>
2020-01-24 12:07:03 +0530

    Implementing case study navigation and fixing fonts

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
 $(document).ready(function () {
 
     $("#btnOpenFitnessCaseStudy").click(function () {
-
-
-
-        $("#fitnesscasestudylistitem").hide(3000);
+        $("#casestudylist").hide('slow');
+        $(".curioustext").hide('slow');
         $("#fitnessCaseStudyDetail").show('slow');
+
+        $(".modal-header").css("background-color", "black");
+        $(".modal-title").css("color", "white");
+        $(".close").css("color", "white");
+
         var bodyStyles = window.getComputedStyle(document.body);
 
         var modalBackgroundColor = bodyStyles.getPropertyValue('--modalbackgroundcolor'); //get
-        console.log(modalBackgroundColor);
-        $(".modal-body").css("background-color", modalBackgroundColor
+        $(".modal-content").css("background-color", modalBackgroundColor
         );
+    });
+
+
+
+    $("#backtocasestudylist").click(function () {
+        $("#casestudylist").show('slow');
+        $(".curioustext").show('slow');
+        $("#fitnessCaseStudyDetail").hide('slow');
+
+        $(".modal-header").css("background-color", "white");
+        $(".modal-title").css("color", "black");
+        $(".close").css("color", "black");
+
+        var bodyStyles = window.getComputedStyle(document.body);
+
+        var modalBackgroundColor = bodyStyles.getPropertyValue('--modalbackgroundcolor'); //get
+        $(".modal-content").css("background-color", "white");
     });
 
 
---
+++ todo.md
@@ -0,0 +1,9 @@
+# DONOTFORGET BEFORE DEPLOYMENT
+
+1. Elements js to be part of webpack.
+2. Favicon
+3. Minify everything
+
+# MUST DO
+
+Full content review
