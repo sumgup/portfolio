ee8ea86de4e719cb1a557837d1c5409b4fcccbd6
sumit gupta <sumit4palz@gmail.com>
2020-01-27 12:55:09 +0530

    Adding my contact section

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,4 +1,4 @@
-var content = 'There are two kind of people, people who create business with there gut feeling and others who validate there assumptions and build the business, which one are you? ';
+var content = 'There are two set of people, people who create business with there gut feeling and others who validate there assumptions and build the business. Which one are you?';
 
 var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
 
@@ -12,6 +12,9 @@ $(ele).hide().appendTo('#typedtext').each(function (i) {
         opacity: 1
     }, 100);      
 
-}).promise().done(function(){ $(".landinglinks").show(1000)});
+}).promise().done(function(){
+     $(".landinglinks").show(4000);
+     $("#typedtext").hide(8000);
+    });
 
 
