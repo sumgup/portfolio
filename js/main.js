36a9430d9a22a8c9c6fc51dee6ab8567533a835a
sumit gupta <sumit4palz@gmail.com>
2020-03-06 12:30:43 +0530

    Generated by grunt gh-pages

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
 $(document).ready(function () {
 
-    $("#btnOpenFitnessCaseStudy").click(function () {
 
+    $("#btn2").click(function () {
+       
         let duration = 1500;
         $("#casestudylist").hide(0);
-        $(".curioustext").hide(0);
+        $("#sketchNoteCaseStudyDetail").show(duration);
+        var bodyStyles = window.getComputedStyle(document.body);
+    });
+
+
+    $("#btnOpenFitnessCaseStudy").click(function () {
+        let duration = 1500;
+        $("#casestudylist").hide(0);
         $("#fitnessCaseStudyDetail").show(duration);
         var bodyStyles = window.getComputedStyle(document.body);
     });
 
 
-
     $("#backtocasestudylist").click(function () {
         let duration = 500;
         $("#casestudylist").show(duration);
-        $(".curioustext").show(0);
        $("#fitnessCaseStudyDetail").hide(0);
     });
+
+    $("#btn3").click(function () {
+        let duration = 500;
+        $("#casestudylist").show(duration);
+       $("#sketchNoteCaseStudyDetail").hide(0);
+    });
+    
 
   // Hide the menu
     $('.navbar-nav>li>a').on('click', function(){
